262ce22665d813fbb4bc33548ee9f544ed5de19c
KHALILULLAH AL FAATH <khalilullah.alfaath21@gmail.com>
2022-06-07 13:25:04 +0700

    testing

--- index.js
+++ index.js
@@ -7,11 +7,27 @@ const alfabet = [...'abcdefghijklmnopqrstuvwxyz'];
 const stateList = [];
 
 let N = 49;
-
 for (let i = 0; i <= N; i++){
     var strAngka = i.toString();
     var str = "q";
     var namaState = str.concat(strAngka);
     stateList.push(namaState)
 }
-console.log(stateList)
+
+stateList.forEach(state => {
+    alfabet.forEach(alfabet => {
+        transitionTable[[state, alfabet]] = 'ERROR'
+    });
+    transitionTable[[state, '#']] = 'ERROR'
+    transitionTable[[state, ' ']] = 'ERROR'
+});
+
+// initial state
+transitionTable[["q0"," "]] = "q0";
+
+// final state
+transitionTable[["q38"," "]] = "q39";
+transitionTable[["q38","#"]] = "ACCEPT";
+transitionTable[["q39"," "]] = "q39";
+transitionTable[["q39","#"]] = "ACCEPT";
+

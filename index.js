79febde469538ca7766db73588be00f678a369e5
Harvian Khusnan Hafidz <71773895+HarvianKhusnan@users.noreply.github.com>
2022-06-07 14:29:03 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -31,3 +31,39 @@ transitionTable[["q38","#"]] = "ACCEPT";
 transitionTable[["q39"," "]] = "q39";
 transitionTable[["q39","#"]] = "ACCEPT";
 
+//Subject мать
+transitionTable[["q0","м"]] = "q1"
+transitionTable[["q1","а"]] = "q2"
+transitionTable[["q2","т"]] = "q3"
+transitionTable[["q3","ь"]] = "q4"
+
+//subject отец
+transitionTable[["q0","о"]] = "q6"
+transitionTable[["q6","т"]] = "q7"
+transitionTable[["q7","e"]] = "q8"
+transitionTable[["q8","ц"]] = "q4"
+
+//subject я
+transitionTable[["q0","я"]] = "q9"
+
+//subject ты
+transitionTable[["q0","т"]] = "q10"
+transitionTable[["q10","ы"]] = "q4"
+
+//subject вы
+transitionTable[["q0","в"]] = "q11"
+transitionTable[["q11","ы"]] = "q4"
+
+//Verb готовит
+transitionTable[["q12","г"]] = "q13"
+transitionTable[["q13","о"]] = "q51"
+transitionTable[["q51","т"]] = "q14"
+transitionTable[["q14","о"]] = "q15"
+transitionTable[["q15","в"]] = "q16"
+transitionTable[["q16","и"]] = "q52"
+transitionTable[["q52","т"]] = "q17"
+
+//verb учить
+transitionTable[["q12","у"]] = "q19"
+transitionTable[["q19","ч"]] = "q20"
+

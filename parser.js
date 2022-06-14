02632d860fdf35031e8124582bf93409aaa1f12c
KHALILULLAH AL FAATH <khalilullah.alfaath21@gmail.com>
2022-06-14 12:35:16 +0700

    Update parser.js

--- parser.js
+++ parser.js
@@ -42,4 +42,68 @@ parse_table[["OB",'книга']] = ['книга']
 parse_table[["OB",'комар']] = ['комар']
 parse_table[["OB",'pом']] = ['pом']
 parse_table[["OB",'ученик']] = ['ученик']
-parse_table[["OB",'EOS']] = ['EOS']
+parse_table[["OB",'EOS']] = ['error']
+
+formParser.onsubmit = (event) => {
+    event.preventDefault();
+    var inputData = inputKalimatParser.value.toLowerCase();
+
+    processParser(inputData.split(' '), inputData)
+} 
+
+var processParser = (tokens, sentences) => {
+    // stack initialization
+    var stack = [];
+    stack.push('#');
+    stack.push('I');
+
+    tokens.push('EOS')
+    console.log('tokens: ', tokens)
+    var idxToken = 0;
+    var symbol = tokens[idxToken];
+
+    while(stack.length > 0) {
+        var top = stack[stack.length - 1];
+        console.log('top =', top);
+        console.log('symbol =', symbol);
+        if(terminals.includes(top)) {
+            console.log('top adalah simbol terminal');  
+            if(top == symbol) {
+                stack.pop();
+                idxToken += 1;
+                symbol = tokens[idxToken];
+                if(symbol == 'EOS') {
+                    console.log('isi stack: ', stack);
+                    stack.pop();
+                }
+            }
+        } else if(nonTerminals.includes(top)) {
+            console.log('top adalah simbol non terminal');
+            if(parse_table[[top, symbol]][0] != 'error') {
+                stack.pop();
+                var symbolToBePushed = parse_table[[top, symbol]];
+                console.log('symbol to be pushed: ', symbolToBePushed);
+                for(let i = symbolToBePushed.length - 1; i > -1; i--) {
+                    stack.push(symbolToBePushed[i]);
+                }
+            } else {
+                console.log('error');
+                break;
+            }
+        } else {
+            console.log('error');
+            break;
+        }
+        console.log('isi stack: ', stack);
+    }
+
+    // conclusion
+    console.log('konklusi');
+    if(symbol == 'EOS' && stack.length == 0) {
+        console.log('Input string ', sentences, 'diterima sesuai grammar');
+        hasilParser.value = `Input string ${sentences} diterima sesuai grammar`;
+    } else {
+        console.log('error, tidak diterima karena tidak sesuai grammar');
+        hasilParser.value = 'error, tidak diterima karena tidak sesuai grammar';
+    }
+}

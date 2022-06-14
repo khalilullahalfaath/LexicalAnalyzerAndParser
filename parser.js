a502d42ef7824449d1a0674a2e096ac7d3bd4e76
Harvian Khusnan Hafidz <harvian7x@gmail.com>
2022-06-14 12:21:09 +0700

    Update parser.js

--- parser.js
+++ parser.js
@@ -4,3 +4,26 @@ var clearParser = document.getElementById('btn-clear-parser');
 var loadingParser = document.getElementById('loading-parser');
 var form = document.getElementById('form-parser'); 
 
+var nonTerminals = ['S', 'SB','VB','OB'];
+var Terminals = ['мать', 'отец','им', 'ты','вы','готовит','уяить','рисовать',
+                  'читать','книга','комар', 'pом', 'ученик' ];
+
+var parse_table = []; 
+
+//S 
+parse_table[['S','мать']] = ['SB', 'VB', 'OB']
+parse_table[['S','отец']] = ['SB', 'VB', 'OB']
+parse_table[['S','им']] = ['SB', 'VB', 'OB']
+parse_table[['S', 'ты']] = ['SB', 'VB', 'OB']
+parse_table[['S','вы']] = ['SB','VB','OB']
+parse_table[['S','готовит']] = ['error']
+parse_table[['S','уяить']] = ['error']
+parse_table[['S','рисовать']] = ['error']
+parse_table[['S','читать']] = ['error']
+parse_table[['S','книга']] = ['error']
+parse_table[['S','комар']] = ['error']
+parse_table[['S','pом']] = ['error']
+parse_table[['S','ученик']] = ['error']
+parse_table[['S','EOS']] = ['error']
+
+//SB

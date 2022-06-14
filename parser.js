a8b9c3b0f29cec55a41d9e87f54fb1729e7bdeb7
Harvian Khusnan Hafidz <harvian7x@gmail.com>
2022-06-14 12:00:14 +0700

    Update parser.js

---
+++ parser.js
@@ -0,0 +1,6 @@
+var InputKalimatParser = document.getElementById('input_kalimat_parser');
+var HasilParser = document.getElementById('result-parser');
+var clearParser = document.getElementById('btn-clear-parser');
+var loadingParser = document.getElementById('loading-parser');
+var form = document.getElementById('form-parser'); 
+
